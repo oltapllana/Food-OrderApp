6f505bf8a45d085df3ad53c2fb82d8daa239a3d7
oltap <oltap@LAPTOP-RAUC0V1F>
2023-02-26 17:00:36 +0100

    some style

--- src/components/UI/Popup.js
+++ src/components/UI/Popup.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "./Button";
 import styled from "./Popup.module.css";
 import style from "../Meals.module.css";
-import btnStyle from './Button.module.css'
+import btnStyle from "./Button.module.css";
 const Popup = (props) => {
   const [show, setShow] = useState(true);
   const closePopup = () => {
@@ -11,10 +11,10 @@ const Popup = (props) => {
   };
   console.log(props.selectedMeals[0].price);
 
-  const sum = props.selectedMeals.map(datum => datum.amount * datum.price).reduce((a, b) => a + b)
+  const sum = props.selectedMeals
+    .map((datum) => datum.amount * datum.price)
+    .reduce((a, b) => a + b);
   console.log(sum);
- 
- 
 
   console.log(props.selectedMeals);
   return (
@@ -24,23 +24,30 @@ const Popup = (props) => {
           <ul className={style.meals_container2}>
             {props.selectedMeals.map((meal) => {
               return (
-                <li key={meal.id}>
+                <li key={meal.id} className={styled.li}>
                   <div className={style.meal_container}>
                     <h3>{meal.name}</h3>
-                    <div className={style.meal__price}>{meal.price}</div>
+                    <div className={styled.popup__amounts}>
+                      <div className={style.meal__price}>{meal.price}</div>
+                      <div className={styled.popup__amount}>
+                        x {meal.amount}
+                      </div>
+                    </div>
                   </div>
-                  <div className={`${style.line} ${style.line__pop}`}></div>
-
                 </li>
-
               );
             })}
-            <div className={styled.popup__actions}>
-              <div><h2>Total Amount</h2> {sum}</div>
-              <Button onClick={closePopup} className={btnStyle.popup__close}>Close</Button>
-              <Button>Order</Button>
+            <div className={styled.popup__totalamount}>
+              <h2>Total Amount</h2>
+              <span>${sum.toFixed(2)}</span>
             </div>
 
+            <div className={styled.popup__actions}>
+              <Button onClick={closePopup} className={btnStyle.popup__close}>
+                Close
+              </Button>
+              <Button>Order</Button>
+            </div>
           </ul>
         </div>
       )}
